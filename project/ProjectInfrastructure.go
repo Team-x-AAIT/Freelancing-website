5a771aa9975ff5f710a9785888973d3fb05cbb10
Yenework123 <51898326+Yenework123@users.noreply.github.com>
2020-01-10 17:56:12 +0300

    Project infrastructure until removing project

--- project/ProjectInfrastructure.go
+++ project/ProjectInfrastructure.go
@@ -1,7 +1,13 @@
 package project
 
 import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
 	"strconv"
 
 	"github.com/Team-x-AAIT/Freelancing-website/entities"
@@ -73,5 +79,101 @@ func PostProject(w http.ResponseWriter, r *http.Request) {
 	user.Temp.ExecuteTemplate(w, "Dashboard.html", loggedInUser)
 	//Don't forget to change it to ajax request.
 	// w.Write([]byte("okay"))
+
+}
+
+// OnCreateProject is a Handler func that display a new page for creating a new project.
+func OnCreateProject(w http.ResponseWriter, r *http.Request) {
+
+	loggedInUser := user.Authentication(r)
+	if loggedInUser == nil {
+		http.Redirect(w, r, "/Login_Page", http.StatusSeeOther)
+		return
+	}
+	user.Temp.ExecuteTemplate(w, "PostProject.html", loggedInUser)
+}
+
+// ReviewProject is a Handler func that display the created project for editing.
+func ReviewProject(w http.ResponseWriter, r *http.Request) {
+
+	loggedInUser := user.Authentication(r)
+	if loggedInUser == nil {
+		http.Redirect(w, r, "/Login_Page", http.StatusSeeOther)
+		return
+	}
+
+	pid := r.FormValue("pid")
+	project := PService.SearchProjectByID(pid)
+	user.Temp.ExecuteTemplate(w, "ReviewProject.html", project)
+
+}
+
+// UpdateProject is a Handler func that initaite project updating process.
+func UpdateProject(w http.ResponseWriter, r *http.Request) {
+
+	loggedInUser := user.Authentication(r)
+	if loggedInUser == nil {
+		http.Redirect(w, r, "/Login_Page", http.StatusSeeOther)
+		return
+	}
+
+	pid := r.FormValue("pid")
+	title := r.FormValue("title")
+	description := r.FormValue("description")
+	details := r.FormValue("details")
+	category := r.FormValue("category")
+	subcategory := r.FormValue("subcategory")
+	budgetS := r.FormValue("budget")
+	worktypeS := r.FormValue("worktype")
+	// Is a string that contain the names of file that are needed to removed.
+	attachedFilesR := r.FormValue("attachedFilesR")
+
+	budget, err := strconv.ParseFloat(budgetS, 0)
+	if err != nil {
+		panic(err)
+	}
+	worktype, err := strconv.ParseInt(worktypeS, 0, 0)
+	if err != nil {
+		panic(err)
+	}
+
+	projectHolder := entities.NewProject(title, description, details, category, subcategory, budget, worktype)
+
+	projectHolder.ID = pid
+	pid, err = PService.UpdateProject(projectHolder)
+	if err != nil {
+		panic(err)
+	}
+
+	ResourceExtractor(pid, "files", "project", r)
+
+	var beginIndex int
+	for index, char1 := range attachedFilesR {
+		if char1 == ',' {
+			filename := attachedFilesR[beginIndex:index]
+			if err = RemoveAsset(pid, filename); err != nil {
+				panic(err)
+			}
+			beginIndex = index + 1
+		}
+	}
+
+}
+
+// RemoveProject is a Handler func that initaite project Removing process.
+func RemoveProject(w http.ResponseWriter, r *http.Request) {
+
+	loggedInUser := user.Authentication(r)
+	if loggedInUser == nil {
+		http.Redirect(w, r, "/Login_Page", http.StatusSeeOther)
+		return
+	}
+
+	uid := loggedInUser.UID
+	pid := r.FormValue("pid")
+
+	if err := PService.RemoveProjectInformation(uid, pid); err != nil {
+		panic(err)
+	}
 
 }
